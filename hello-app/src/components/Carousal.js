b840c1c8cb9ce716f1425985e8f2ede48b835ec7
shrenik <shrenik@gmail.com>
2026-02-21 17:50:26 +0530

    Carousal Component Created

--- hello-app/src/components/Carousal.js
+++ hello-app/src/components/Carousal.js
@@ -2,8 +2,20 @@
 
 let Caraousal = () => {
   return (
-    <div>
-        <h1>Caraousal Component</h1>
+    <div className="flex">
+      <marquee>
+        <img src="https://picsum.photos/id/1/150/150" className="m-3 rounded-xl" style={{ display: "inline-block" }} />
+        <img src="https://picsum.photos/id/50/150/150" className="m-3 rounded-xl" style={{ display: "inline-block" }} />
+        <img src="https://picsum.photos/id/100/150/150" className="m-3 rounded-xl" style={{ display: "inline-block" }} />
+        <img src="https://picsum.photos/id/450/150/150" className="m-3 rounded-xl" style={{ display: "inline-block" }} />
+        <img src="https://picsum.photos/id/200/150/150" className="m-3 rounded-xl" style={{ display: "inline-block" }} />
+        <img src="https://picsum.photos/id/250/150/150" className="m-3 rounded-xl" style={{ display: "inline-block" }} />
+        <img src="https://picsum.photos/id/300/150/150" className="m-3 rounded-xl" style={{ display: "inline-block" }} />
+        <img src="https://picsum.photos/id/350/150/150" className="m-3 rounded-xl" style={{ display: "inline-block" }} />
+        <img src="https://picsum.photos/id/400/150/150" className="m-3 rounded-xl" style={{ display: "inline-block" }} />
+        <img src="https://picsum.photos/id/450/150/150" className="m-3 rounded-xl" style={{ display: "inline-block" }} />
+      </marquee>
+
     </div>
   )
 }
--- hello-app/src/components/Header.js
+++ hello-app/src/components/Header.js
@@ -10,11 +10,11 @@ let Header = () => {
           <span className="bg-white text-red-900 mx-1 rounded-md p-1 text-xl">my</span>
           <span className=" mx-1">trip</span>
         </li>
-        <li><i class="fa-solid fa-bell-concierge fa-flip" style={{"color": "rgb(255, 255, 255)"}}></i> Hotels</li>
-        <li><i class="fa-solid fa-plane" style={{"color": "rgb(255, 255, 255)"}}></i>Flights</li>
-        <li><i class="fa-solid fa-train-subway fa-fade" style={{"color": "rgb(255, 255, 255)"}}></i> Train</li>
-        <li><i class="fa-solid fa-bus fa-beat-fade" style={{"color": "rgb(255, 255, 255)"}}></i> Bus</li>
-        <li><i class="fa-solid fa-arrow-right-to-bracket fa-spin fa-spin-reverse" style={{"color": "rgb(255, 255, 255)"}}></i> Login</li>
+        <li><i class="fa-solid fa-bell-concierge fa-flip" style={{ "color": "rgb(255, 255, 255)" }}></i> Hotels</li>
+        <li><i class="fa-solid fa-plane" style={{ "color": "rgb(255, 255, 255)" }}></i>Flights</li>
+        <li><i class="fa-solid fa-train-subway fa-fade" style={{ "color": "rgb(255, 255, 255)" }}></i> Train</li>
+        <li><i class="fa-solid fa-bus fa-beat-fade" style={{ "color": "rgb(255, 255, 255)" }}></i> Bus</li>
+        <li><i class="fa-solid fa-arrow-right-to-bracket fa-spin fa-spin-reverse" style={{ "color": "rgb(255, 255, 255)" }}></i> Login</li>
       </ul>
     </div>
   )
