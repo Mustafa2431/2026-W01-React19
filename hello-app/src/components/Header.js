c84554b0dc43076c5f3c78d80226614326dadb71
shrenik <shrenik@gmail.com>
2026-02-21 17:16:42 +0530

    React Project Make My Trip

---
+++ hello-app/src/components/Header.js
@@ -0,0 +1,23 @@
+
+
+
+let Header = () => {
+  return (
+    <div>
+      <h1>Header Component</h1>
+    </div>
+  )
+}
+
+
+let Demo = () =>{
+    return (
+        <>
+        </>
+    )
+}
+
+export {
+    Header,
+    Demo
+}
---
+++ hello-app/src/components/Offers.js
@@ -0,0 +1,12 @@
+
+
+let Offers = () => {
+  return (
+    <div>
+        <h1>offer Component</h1>
+    </div>
+  )
+}
+
+
+export default Offers;
